ce9699b60f6c7130e7f00bbbad1a3b6a89ecdbba
Ahmad Rasoulpour <ahmad.raman83@yahoo.com>
2019-04-28 14:01:43 -0600

    factorial recursion

--- factorial/index.js
+++ factorial/index.js
@@ -5,10 +5,4 @@ function factorial(n){
     return n * factorial(n-1)
 }
 
-// console.log(factorial(5))
-// console.log(factorial(4))
-// console.log(factorial(3))
-// console.log(factorial(2))
-// console.log(factorial(1))
-
 module.exports = factorial
