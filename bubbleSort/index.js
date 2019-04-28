554af9a41182dbfc847288da3cdb02c44f5ca003
Ahmad Rasoulpour <ahmad.raman83@yahoo.com>
2019-04-28 13:00:53 -0600

    bubble sort

--- bubbleSort/index.js
+++ bubbleSort/index.js
@@ -1,20 +1,21 @@
 
-let swapped
+// let swapped
 function bubbleSort(arr){
-    do{
-    swapped = false
-    let end = arr.length - 1
-    for(let i = 0; i < end; i++){
-        if(arr[i] > arr[i+1]){
-            swapped = true
-            let temp = arr[i]
-            arr[i] = arr[i+1]
-            arr[i+1] = temp
+    let isSorted = false
+    while(!isSorted){
+        isSorted = true
+        let end = arr.length - 1
+        for(let i = 0; i < end; i++){
+            if(arr[i] > arr[i+1]){
+                let temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                isSorted = false
+            }
         }
+        end--
     }
-    end--
-} while(swapped)
-return arr
+    return arr
 }
 
 module.exports = bubbleSort
